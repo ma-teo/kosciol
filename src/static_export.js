cfa3f3dc8e47545fb80ef24abf5d272d6096b3ab
mateo <mateo.tarasiuk@gmail.com>
2020-12-22 15:35:02 +0100

    Update static export script

--- src/static_export.js
+++ src/static_export.js
@@ -1,14 +1,19 @@
 import 'regenerator-runtime/runtime'
+import fs from 'fs'
 import axios from 'axios'
 import { apiUrl } from './components/utils/context'
 import { renderApp } from './server'
 
-export const render = async (req, res) => {
-  const {data} = await axios(`${apiUrl}/meta`)
-  const html = renderApp({req, api: data})
-  res.json({html})
+export const render = async reqs => {
+  for (const req of reqs) {
+    const {data} = await axios(`${apiUrl}/meta`)
+    const html = renderApp({req, api: data})
+    fs.writeFileSync(`${process.env.STATIC_PATH}${req}.html`, html)
+  }
 }
 
-export const routes = () => {
-  return ['static']
-}
+export const reqs = [
+  '/200'
+]
+
+export default render(reqs)
