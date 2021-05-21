9297709de97b975189c3a57dc56cecac5975b6e6
mateo <mateo.tarasiuk@gmail.com>
2021-05-21 11:12:05 +0200

    Update dependencies and workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js')
 
 self.skipWaiting()
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
